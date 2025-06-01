64330bc4d39bf16d9f1cb675a4c870134f1f350f
trigonyx <factor_adore_3m@icloud.com>
2025-06-01 13:56:37 +0200

    Made getters of Particle properties public

--- Sources/Vortex/System/Particle.swift
+++ Sources/Vortex/System/Particle.swift
@@ -12,41 +12,41 @@ extension VortexSystem {
     public struct Particle: Hashable {
         /// The tag for this particle, which should match one of the tags attached to
         /// the SwiftUI views you're passing into a `VortexView`.
-        var tag: String
+        private(set) var tag: String
 
         /// The current location of this particle, specified in unit space.
-        var position: SIMD2<Double>
+        private(set) var position: SIMD2<Double>
 
         /// The current speed of this particle.
-        var speed: SIMD2<Double>
+        private(set) var speed: SIMD2<Double>
         
         /// The initial speed the particle was created at
-        var initialSpeed: SIMD2<Double>
+        private(set) var initialSpeed: SIMD2<Double>
 
         /// The time this particle was created.
-        var birthTime: TimeInterval
+        private(set) var birthTime: TimeInterval
 
         /// How long this particle should live for, measured in seconds.
-        var lifespan: TimeInterval
+        private(set) var lifespan: TimeInterval
 
         /// The initial size this particle was created at.
-        var initialSize: Double
+        private(set) var initialSize: Double
 
         /// The current size of this particle. This is recomputed every time its system's
         /// `update()` method is called.
-        var currentSize = 0.0
+        private(set) var currentSize = 0.0
 
         /// The rotation angle of this particle.
-        var angle = SIMD3<Double>()
+        private(set) var angle = SIMD3<Double>()
 
         /// How fast this particle is spinning.
-        var angularSpeed = SIMD3<Double>()
+        private(set) var angularSpeed = SIMD3<Double>()
 
         /// The colors to use for rendering this particle over time.
-        var colors: [Color]
+        private(set) var colors: [Color]
 
         /// The current color to use for rendering this particle right now. This is recomputed
         /// every time its system's `update()` method is called.
-        var currentColor = Color.white
+        private(set) var currentColor = Color.white
     }
 }
